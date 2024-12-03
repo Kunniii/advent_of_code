f130c6e25d11cddb0b4562a5f937773e2748e69d
Kunniii <57139436+Kunniii@users.noreply.github.com>
2024-12-03 12:52:59 +0700

    first try

---
+++ MMXXIV/3/p2.py
@@ -0,0 +1,30 @@
+from p1 import loadFile, mul
+import re
+
+def solution(inp: list[str]):
+    pattern = "mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)"
+    total = 0
+    for line in inp:
+        skip = False
+        matches = re.findall(pattern, line)
+        for match in matches:
+            if match == "don't()":
+                skip = True
+            elif match == "do()":
+                skip = False
+            print(not skip, match)
+            if skip:
+                continue
+            else:
+                if match.startswith('mul'):
+                    total += mul(match)
+    return total
+
+
+
+
+if __name__ == "__main__":
+    inp = loadFile()
+    fh = open("output.txt", "w+")
+    fh.write(str(solution(inp)))
+    fh.close()
