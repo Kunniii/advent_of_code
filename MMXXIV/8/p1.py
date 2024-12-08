d62770929ea71edb90bf7237ed8ff3a13d5be253
Kunniii <57139436+Kunniii@users.noreply.github.com>
2024-12-09 00:54:12 +0700

    d8 p1

--- MMXXIV/8/p1.py
+++ MMXXIV/8/p1.py
@@ -1,5 +1,6 @@
 from sys import argv
 from logger import Logger
+import itertools
 
 logger = Logger()
 log = logger.log
@@ -16,9 +17,70 @@ def loadFile() -> list[str]:
     return lines
 
 
+class Node:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def location(self):
+        return (self.x, self.y)
+
+    def apply(self, x: int, y: int):
+        return (self.x + x, self.y + y)
+
+    def __repr__(self):
+        return f"(x,y)=({self.x},{self.y})"
+
+
+def how_to_go_from_a_to_b(a: Node, b: Node):
+    x1, y1 = a.location()
+    x2, y2 = b.location()
+    return (x2 - x1, y2 - y1)
+
+
 def solution(inp: list[str]):
-    log("Debug")
-    return inp
+    H, W = len(inp), len(inp[0])
+    nodes_map: dict[str, list[Node]] = {}
+
+    board = [list(i) for i in inp]
+
+    for y in range(H):
+        for x in range(W):
+            c = inp[y][x]
+            if c != ".":
+                node_with_type = nodes_map.get(c, [])
+                node_with_type.append(Node(x, y))
+                nodes_map[c] = node_with_type
+
+    # anti_nodes: set[tuple[int, int]] = set()
+
+    total = 0
+
+    for nodes in nodes_map.values():
+        for [nodeA, nodeB] in itertools.product(nodes, repeat=2):
+            if nodeA == nodeB:
+                continue
+            (x, y) = how_to_go_from_a_to_b(nodeA, nodeB)
+            (x2, y2) = nodeA.apply(x * 2, y * 2)
+            if 0 <= x2 < W and 0 <= y2 < H:
+                log(f"OK {x2}, {y2} - {W}, {H}")
+                board[y2][x2] = "#"
+            else:
+                log(f"NOT {x2}, {y2} - {W}, {H}")
+
+    log()
+
+    for line in board:
+        log("".join(line))
+        total += line.count("#")
+
+    # for nodes in nodes_map.values():
+    #     for node in nodes:
+    #         anti_nodes.discard(node.location())
+
+    # return len(anti_nodes)
+
+    return total
 
 
 if __name__ == "__main__":
