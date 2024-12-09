0b4b2166acce3a7e011807791808793152618aea
Kunniii <57139436+Kunniii@users.noreply.github.com>
2024-12-09 13:52:56 +0700

    wip: d9 p1

--- MMXXIV/9/p1.py
+++ MMXXIV/9/p1.py
@@ -17,8 +17,48 @@ def loadFile() -> list[str]:
 
 
 def solution(inp: list[str]):
-    log("Debug")
-    return inp
+    disk_map = inp[0]
+
+    files: dict[int, int] = {}
+    free_spaces: dict[int, int] = {}
+    blocks: list[str] = []
+    isFile = True
+    for i in range(len(disk_map)):
+        log("Creating file system", cmd=True)
+        if isFile:
+            file = int(disk_map[i])
+            files[i] = file
+            for f in range(1, file+1):
+                blocks.append(str(i // 2))
+        else:
+            free_space = int(disk_map[i])
+            free_spaces[i] = free_space
+            blocks += list("." * free_space)
+        isFile = not isFile
+
+    len_blocks = len(blocks)
+    log((blocks))
+
+    for i in range(len_blocks):
+        if blocks[i] == ".":
+            for j in range(len_blocks - 1, i, -1):
+                if blocks[j] != ".":
+                    log(f"Swapping {i} - {j} ({blocks[j]})", cmd=True)
+                    t = blocks[i]
+                    blocks[i] = blocks[j]
+                    blocks[j] = t
+                    break
+    log((blocks))
+
+    total = 0
+
+    for i in range(len_blocks):
+        s = blocks[i]
+        if s.isnumeric():
+            log(f"Calc {s}*{i}", cmd=True)
+            total += int(f) * i
+
+    return total
 
 
 if __name__ == "__main__":
