3dbca10d0dcd971ae812d36cf10cbf7d80f21384
Kunniii <57139436+Kunniii@users.noreply.github.com>
2024-12-09 13:52:52 +0700

    update template

--- MMXXIV/9/logger.py
+++ MMXXIV/9/logger.py
@@ -2,8 +2,10 @@ class Logger:
     def __init__(self) -> None:
         self.log_file = open("debug.log", "w+")
 
-    def log(self, *objects, sep=" ", end="\n", flush=False):
+    def log(self, *objects, sep=" ", end="\n", flush=False, cmd=False):
         print(*objects, sep=sep, end=end, file=self.log_file, flush=flush)
+        if cmd:
+            print(*objects, sep=sep, end=end, flush=flush)
 
     def __del__(self):
         self.log_file.close()
